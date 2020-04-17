ea0283dc98bf9700f6d9817352991bd4814f26bb
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-04-17 16:20:57 +0530

    XAMARIN-33991 - Sample added

---
+++ TreeViewXamarin/TreeViewXamarin/TreeViewXamarin/Helper/Behavior.cs
@@ -0,0 +1,29 @@
+﻿using Syncfusion.XForms.TreeView;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TreeViewXamarin
+{
+    public class Behavior : Behavior<SfTreeView>
+    {
+        protected override void OnAttachedTo(SfTreeView bindable)
+        {
+            bindable.QueryNodeSize += Bindable_QueryNodeSize;
+            base.OnAttachedTo(bindable);
+        }
+
+        private void Bindable_QueryNodeSize(object sender, QueryNodeSizeEventArgs e)
+        {
+            // Returns item height based on the content loaded.
+            e.Height = e.GetActualNodeHeight();
+            e.Handled = true;
+        }
+        protected override void OnDetachingFrom(SfTreeView bindable)
+        {
+            bindable.QueryNodeSize -= Bindable_QueryNodeSize;
+            base.OnDetachingFrom(bindable);
+        }
+    }
+}
